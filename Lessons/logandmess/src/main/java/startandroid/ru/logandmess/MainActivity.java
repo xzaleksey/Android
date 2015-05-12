0514b0bacf497af710e545dd8b018290546d90bc
xzaleksey <xaleksey@gmail.com>
2015-05-12 15:44:57 +0300

    Работа с кнопками

---
+++ Lessons/logandmess/src/main/java/startandroid/ru/logandmess/MainActivity.java
@@ -0,0 +1,75 @@
+package startandroid.ru.logandmess;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    private static final String TAG = "myLogs";
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "найдем View-элементы");
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        // присваиваем обработчик кнопкам
+        Log.d(TAG, "присваиваем обработчик кнопкам");
+        btnOk.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d(TAG, "по id определяем кнопку, вызвавшую этот обработчик");
+        switch (v.getId()) {
+            case R.id.btnOk:
+                // кнопка ОК
+                Log.d(TAG, "кнопка ОК");
+                tvOut.setText("Нажата кнопка ОК");
+                Toast.makeText(this, "Нажата кнопка ОК", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnCancel:
+                // кнопка Cancel
+                Log.d(TAG, "кнопка Cancel");
+                tvOut.setText("Нажата кнопка Cancel");
+                Toast.makeText(this, "Нажата кнопка Cancel", Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+}
